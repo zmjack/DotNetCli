137f5a76be2d6d094eb8ecd0b6b55aec8f16f64f
zmjack <jack@zmland.com>
2021-10-14 16:47:17 +0800

    v0.1.8

--- DotNetCli/CmdContainer.cs
+++ DotNetCli/CmdContainer.cs
@@ -84,6 +84,11 @@ Commands:");
                 }
                 else PrintUsage();
             }
+            catch (Exception ex)
+            {
+                var innerMostException = ex.Forward(x => x.InnerException, x => x.InnerException is null);
+                Console.WriteLine(innerMostException.Message);
+            }
             finally
             {
                 try { Console.CursorVisible = true; }
